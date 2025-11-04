6d4f9bf1ab0afcc82bf66317272914a5f64b8018
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-04 19:09:21 +0530

    Installed react redux and redux toolkit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,21 @@ import viteLogo from "/vite.svg";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Body from "./Body";
 import Login from "./Login";
+import {Provider} from "react-redux";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    <Provider>
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Body />} />
             <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
+    </Provider>
     </>
   );
 }
